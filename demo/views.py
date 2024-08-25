a763546e484a9a6fdd7f50cdcb298bf561d8a051
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2024-08-25 19:19:43 +0300

    Permissions added

--- demo/views.py
+++ demo/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 from .models import Book
 from .serializers import BookSerilizer
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerilizer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
